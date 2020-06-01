d953080d644312e98f3b80b34f0f955adfffa7c7
shubh <ror.shubh@gmail.com>
2020-06-01 23:55:46 +0530

    Updates

---
+++ precache-manifest.af1eb02b9e28e2433f48669b7c12f518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffdc47cf1572cdd637daf77bc7246fc",
+    "url": "/adpushup/index.html"
+  },
+  {
+    "revision": "6a5f01254dbb6f330306",
+    "url": "/adpushup/static/css/main.18fda380.chunk.css"
+  },
+  {
+    "revision": "82f4d928b48c42d2b541",
+    "url": "/adpushup/static/js/2.8fbc1486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adpushup/static/js/2.8fbc1486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5f01254dbb6f330306",
+    "url": "/adpushup/static/js/main.9c954fbd.chunk.js"
+  },
+  {
+    "revision": "055c4c7de25d25f0b154",
+    "url": "/adpushup/static/js/runtime-main.ed2c1a07.js"
+  }
+]);
